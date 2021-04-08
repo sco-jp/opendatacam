a1311547eca503995516f7f50e514037d38bbb92
Jaroslaw Piwonski <piwonski@smart-city-operations.com>
2021-04-08 09:48:13 +0200

    added basic auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     env: {
       basePath: '/aicc-basic-demo',
+      username: 'admin',
+      password: 'aicc-basic-demo',
     },
     basePath: '/aicc-basic-demo',
     assetPrefix: '/aicc-basic-demo',
